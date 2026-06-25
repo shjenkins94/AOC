0a179ebccf0ad175820a9e86be3cc2d1421a3e7e
Scott H. Jenkins <shjenkins94@gmail.com>
2026-06-25 23:30:44 +0900

    fix install path

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ set -euo pipefail
 # - Bootstraps hyphy-analyses repo if missing
 #
 # Usage:
-#   bash ./install.sh AOC envs/AOC.yaml
+#   bash ./install.sh AOC workflow/envs/AOC.yaml
 #   FRONTEND_OVERRIDE=conda bash ./install.sh AOC envs/AOC.yaml
 #   FRONTEND_OVERRIDE=micromamba bash ./install.sh AOC envs/AOC.yaml
 # -----------------------------------------------------------------------------
 
 ENV_NAME="${1:-AOC}"
-ENV_FILE="${2:-envs/AOC.yaml}"
+ENV_FILE="${2:-workflow/envs/AOC.yaml}"
 
 echo "[install] env_name=${ENV_NAME}"
 echo "[install] env_file=${ENV_FILE}"
