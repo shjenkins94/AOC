2a6358c274b150dc1c33776b75f144038a41dc60
Scott H. Jenkins <shjenkins94@gmail.com>
2026-07-17 22:12:37 +0900

    adding profiles

--- .tests/test_installation.sh
+++ .tests/test_installation.sh
@@ -4,15 +4,14 @@ set -e
 echo "Running AOC installation test..."
 
 # Clean previous test
-rm -rf .tests/integration/results
-rm -rf .tests/integration/logs
+# rm -rf .tests/integration/results
+# rm -rf .tests/integration/logs
 
 # Run minimal workflow
 snakemake \
   --cores 10 \
   --directory .tests/integration \
-  --rerun-trigger mtime \
-  --configfile .tests/integration/config/config_mini_legacy.yaml \
+  --configfile .tests/integration/config/config.yaml \
   --config gard_processors=8 
 # Basic checks
 #if [ ! -f tests/tmp_output/summary/run_manifest.csv ]; then
